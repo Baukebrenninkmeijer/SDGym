b430bcfba4925c2fa9c7969e690d1dc57e9075a9
Bauke <bauke.brenninkmeijer@gmail.com>
2019-07-25 20:38:36 +0200

    Commit the AE stuff

--- sdgym/synthesizers/medgan.py
+++ sdgym/synthesizers/medgan.py
@@ -7,7 +7,7 @@ from torch import nn
 from torch.nn import BatchNorm1d, Linear, Module, Sequential
 from tqdm import trange
 from torch.nn.functional import cross_entropy, mse_loss, sigmoid
-from torch.optim import Adam
+from torch.optim import Adam, SGD
 from torch.utils.data import DataLoader, TensorDataset
 
 # from sdgym.synthesizer_base import SynthesizerBase
@@ -18,7 +18,7 @@ class ResidualFC(Module):
     def __init__(self, input_dim, output_dim, activate, bnDecay):
         super(ResidualFC, self).__init__()
         self.seq = Sequential(
-            Linear(input_dim, output_dim),
+            Linear(input_dim, output_dim, bias=False),
             BatchNorm1d(output_dim, momentum=bnDecay),
             activate()
         )
@@ -30,7 +30,7 @@ class ResidualFC(Module):
 
 class Generator(Module):
     def __init__(self, random_dim, hidden_dim, bnDecay):
-        super(Generator, self).__init__()
+        super().__init__()
 
         dim = random_dim
         seq = []
@@ -39,7 +39,7 @@ class Generator(Module):
             seq += [ResidualFC(dim, dim, nn.ReLU, bnDecay)]
         assert hidden_dim[-1] == dim
         seq += [
-            Linear(dim, dim),
+            Linear(dim, dim, bias=False),
             BatchNorm1d(dim, momentum=bnDecay),
             nn.ReLU()
         ]
@@ -51,7 +51,7 @@ class Generator(Module):
 
 class Discriminator(Module):
     def __init__(self, data_dim, hidden_dim):
-        super(Discriminator, self).__init__()
+        super().__init__()
         dim = data_dim * 2
         seq = []
         for item in list(hidden_dim):
@@ -88,7 +88,7 @@ class Encoder(Module):
 
 class Decoder(Module):
     def __init__(self, embedding_dim, decompress_dims, data_dim):
-        super(Decoder, self).__init__()
+        super().__init__()
         dim = embedding_dim
         seq = []
         for item in list(decompress_dims):
@@ -100,8 +100,38 @@ class Decoder(Module):
         seq.append(Linear(dim, data_dim))
         self.seq = Sequential(*seq)
 
-    def forward(self, input, output_info):
-        return self.seq(input)
+    def forward(self, org_input, output_info, training=False):
+        return self.seq(org_input)
+
+
+#         if training:
+#             return self.seq(org_input)
+#         else:
+#             input = self.seq(org_input)
+#             st = 0
+#             output = []
+#             for item in output_info:
+#                 if item[1] == 'sigmoid':
+#                     ed = st + item[0]
+#                     output.append(sigmoid(input[:, st:ed]))
+#                     st = ed
+#                 elif item[1] == 'softmax':
+#                     ed = st + item[0]
+#                     softmax_out = torch.nn.functional.gumbel_softmax(input[:, st:ed])
+#                     noise = d = torch.randn(item[0]).to("cuda:0") * 0.05
+#                     softmax_smooth = 0.95 * softmax_out + noise * softmax_out
+#                     output.append(softmax_smooth)
+#                     st = ed
+#             cat = torch.cat(output, dim=1)
+#             return cat.view(org_input.shape[0], np.sum([x[0] for x in output_info]))
+
+
+def weights_init_uniform(m):
+    if isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform_(m.weight)
+
+
+#         m.bias.data.fill_(0.01)
 
 
 def aeloss(fake, real, output_info):
@@ -159,38 +189,52 @@ class MedganSynthesizer:
             experiment.log_parameter('generator_dims', self.generator_dims)
             experiment.log_parameter('discriminator_dims', self.discriminator_dims)
             experiment.log_parameter('GAN version', 'MedGAN')
-            
+
         self.transformer = GeneralTransformer(self.meta)
         self.transformer.fit(train_data)
         train_data = self.transformer.transform(train_data)
         dataset = TensorDataset(torch.from_numpy(train_data.astype('float32')).to(self.device))
         loader = DataLoader(dataset, batch_size=self.batch_size, shuffle=True, drop_last=True)
-
         data_dim = self.transformer.output_dim
         encoder = Encoder(data_dim, self.compress_dims, self.embedding_dim).to(self.device)
         decoder = Decoder(self.embedding_dim, self.compress_dims, data_dim).to(self.device)
+        opt = SGD
         optimizerAE = Adam(
             list(encoder.parameters()) + list(decoder.parameters()),
             weight_decay=self.l2scale
         )
+
         print(f'Pretraining encoders')
         for i in trange(self.pretrain_epoch):
             for id_, data in enumerate(loader):
                 optimizerAE.zero_grad()
                 real = data[0].to(self.device)
                 emb = encoder(real)
-                rec = decoder(emb, self.transformer.output_info)
+                rec = decoder(emb, self.transformer.output_info, training=True)
                 loss = aeloss(rec, real, self.transformer.output_info)
                 loss.backward()
+                experiment.log_metric('AELoss', loss)
                 optimizerAE.step()
+
+        #         test_batch = dataset[:2][0]
+        #         print(test_batch)
+        #         encoder_out = encoder(test_batch)
+        #         print(encoder_out)
+        #         print(decoder(encoder_out, self.transformer.output_info))
 
         generator = Generator(self.random_dim, self.generator_dims, self.bnDecay).to(self.device)
         discriminator = Discriminator(data_dim, self.discriminator_dims).to(self.device)
-        optimizerG = Adam(
+        generator.apply(weights_init_uniform)
+        discriminator.apply(weights_init_uniform)
+
+        optimizerG = opt(
             list(generator.parameters()) + list(decoder.parameters()),
-            weight_decay=self.l2scale
+            #             weight_decay=self.l2scale,
+            lr=5e-3,
         )
-        optimizerD = Adam(discriminator.parameters(), weight_decay=self.l2scale)
+        optimizerD = opt(discriminator.parameters(),
+                         #                          weight_decay=self.l2scale,
+                         lr=5e-3)
         print(f'Starting training')
         mean = torch.zeros(self.batch_size, self.random_dim, device=self.device)
         std = mean + 1
